4d9a042ebc9ebc1eca1510b4023f55b332a64523
devalamin <alamin.hasibmd2017@gmail.com>
2025-03-01 10:25:03 +0300

    functionality added

--- js/script.js
+++ js/script.js
@@ -5,30 +5,45 @@ let assignedValue = parseInt(assignedTask.innerText)
 let CompletedValue = parseInt(completedTask.innerText)
 
 const headingDate = document.getElementById('heading-date')
-headingDate.innerText = new Date().toLocaleString()
+headingDate.innerText = new Date().toLocaleDateString('en-us')
 
 const cards = document.querySelectorAll('.card')
 
 
 cards.forEach(card => {
-    const heading = card.querySelector('.title')
+    const heading = card.querySelector('.title');
+    const history = document.getElementById('history')
+    const p = document.createElement('p');
+    p.classList = 'my-2 font-bold'
 
     const button = card.querySelector('.btn')
 
     button.addEventListener('click', function () {
 
-        if (assignedValue <= 1){
-            alert ('Completed Every Task')
-            if(assignedValue ===0){
+        if (assignedValue <= 1) {
+            alert('Completed Every Task')
+            if (assignedValue === 0) {
                 return
             }
-        
+
         }
         assignedValue -= 1
         CompletedValue += 1
 
         assignedTask.innerText = assignedValue
         completedTask.innerText = CompletedValue
-    })
+        alert('Task Completed')
+        button.disabled = true;
 
+        p.innerText = `You Have completed ${heading.innerText} task at ${new Date().toLocaleTimeString()}`;
+        history.appendChild(p)
+
+
+    });
+
+});
+
+document.getElementById('clear-history').addEventListener('click', function () {
+    const historyDiv = document.getElementById('history');
+    historyDiv.innerHTML = '';
 })
