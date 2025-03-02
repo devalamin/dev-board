ec391b7737b565e80e1bdf5ef2b4ae6dab311571
devalamin <alamin.hasibmd2017@gmail.com>
2025-03-02 10:21:16 +0300

    js file switched

--- js/script.js
+++ js/script.js
@@ -1,67 +1,67 @@
-const assignedTask = document.getElementById('assigned-task');
-const completedTask = document.getElementById('completed-task');
+// const assignedTask = document.getElementById('assigned-task');
+// const completedTask = document.getElementById('completed-task');
 
-let assignedValue = parseInt(assignedTask.innerText)
-let CompletedValue = parseInt(completedTask.innerText)
+// let assignedValue = parseInt(assignedTask.innerText)
+// let CompletedValue = parseInt(completedTask.innerText)
 
-const headingDate = document.getElementById('heading-date')
-headingDate.innerText = new Date().toLocaleDateString('en-us',{
-    weekday: 'short', 
-    day: 'numeric', 
-    month: 'short', 
-    year: 'numeric'
-})
+// const headingDate = document.getElementById('heading-date')
+// headingDate.innerText = new Date().toLocaleDateString('en-us',{
+//     weekday: 'short', 
+//     day: 'numeric', 
+//     month: 'short', 
+//     year: 'numeric'
+// })
 
-const cards = document.querySelectorAll('.card')
+// const cards = document.querySelectorAll('.card')
 
 
-cards.forEach(card => {
-    const heading = card.querySelector('.title');
-    const history = document.getElementById('history')
-    const p = document.createElement('p');
-    p.classList = 'my-2 font-bold'
+// cards.forEach(card => {
+//     const heading = card.querySelector('.title');
+//     const history = document.getElementById('history')
+//     const p = document.createElement('p');
+//     p.classList = 'my-2 font-bold'
 
-    const button = card.querySelector('.btn')
+//     const button = card.querySelector('.btn')
 
-    button.addEventListener('click', function () {
+//     button.addEventListener('click', function () {
 
-        assignedValue--;
-        CompletedValue++;
+//         assignedValue--;
+//         CompletedValue++;
 
-        assignedTask.innerText = assignedValue
-        completedTask.innerText = CompletedValue
-        alert('Task Completed')
-        button.disabled = true;
-        button.innerText = 'Completed'
+//         assignedTask.innerText = assignedValue
+//         completedTask.innerText = CompletedValue
+//         alert('Task Completed')
+//         button.disabled = true;
+//         button.innerText = 'Completed'
 
-        p.innerText = `You Have completed ${heading.innerText} task at ${new Date().toLocaleTimeString()}`;
-        history.appendChild(p)
-        if (assignedValue == 0) {
-            alert('You Have Completed Every Task')
+//         p.innerText = `You Have completed ${heading.innerText} task at ${new Date().toLocaleTimeString()}`;
+//         history.appendChild(p)
+//         if (assignedValue == 0) {
+//             alert('You Have Completed Every Task')
             
-        }
+//         }
 
-    });
+//     });
 
-});
-
-
-// clearing history
-document.getElementById('clear-history').addEventListener('click', function () {
-    const historyDiv = document.getElementById('history');
-
-    historyDiv.innerHTML = '';
-});
+// });
 
 
+// // clearing history
+// document.getElementById('clear-history').addEventListener('click', function () {
+//     const historyDiv = document.getElementById('history');
 
-// changing theme dynamically
-const bgColors = ['#edede9', '#d6ccc2', '#f5ebe0', '#e3d5ca', '#d5bdaf', '#e9edc9', '#cce3de'];
-let i = 0;
-document.getElementById('color-change-btn').addEventListener('click', function () {
-    document.body.style.backgroundColor = bgColors[i];
-    // index++
-    i = (i + 1) % bgColors.length
-});
+//     historyDiv.innerHTML = '';
+// });
+
+
+
+// // changing theme dynamically
+// const bgColors = ['#edede9', '#d6ccc2', '#f5ebe0', '#e3d5ca', '#d5bdaf', '#e9edc9', '#cce3de'];
+// let i = 0;
+// document.getElementById('color-change-btn').addEventListener('click', function () {
+//     document.body.style.backgroundColor = bgColors[i];
+//     // index++
+//     i = (i + 1) % bgColors.length
+// });
 
 
---
+++ js/script2.js
@@ -0,0 +1,65 @@
+const assignedTask = document.getElementById('assigned-task');
+const completedTask = document.getElementById('completed-task');
+
+let assignedValue = parseInt(assignedTask.innerText)
+let CompletedValue = parseInt(completedTask.innerText)
+
+const history = document.getElementById('history')
+
+
+// button functionality
+function completeTask(event){
+    
+    const title = event.parentNode.parentNode.children[1].innerText;
+    const button = event;
+
+    const p = document.createElement('p');
+    p.classList = 'my-2 font-bold'
+    
+    assignedValue--;
+    CompletedValue++;
+
+    assignedTask.innerText = assignedValue;
+    completedTask.innerText = CompletedValue;
+
+    alert('Task Completed')
+        button.disabled = true;
+        button.innerText = 'Completed'
+
+        p.innerText = `You Have completed ${title} task at ${new Date().toLocaleTimeString()}`;
+        history.appendChild(p)
+        if (assignedValue == 0) {
+            alert('You Have Completed Every Task')
+            
+        }
+
+
+};
+
+
+
+// // clearing history
+document.getElementById('clear-history').addEventListener('click', function () {
+    const historyDiv = document.getElementById('history');
+
+    historyDiv.innerHTML = '';
+});
+
+
+// dynamic date
+const headingDate = document.getElementById('heading-date')
+headingDate.innerText = new Date().toLocaleDateString('en-us',{
+    weekday: 'short', 
+    day: 'numeric', 
+    month: 'short', 
+    year: 'numeric'
+});
+
+// changing theme dynamically
+const bgColors = ['#edede9', '#d6ccc2', '#f5ebe0', '#e3d5ca', '#d5bdaf', '#e9edc9', '#cce3de'];
+let i = 0;
+document.getElementById('color-change-btn').addEventListener('click', function () {
+    document.body.style.backgroundColor = bgColors[i];
+    // index++
+    i = (i + 1) % bgColors.length
+});
